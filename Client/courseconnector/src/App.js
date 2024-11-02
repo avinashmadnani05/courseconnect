4ef2153a21d23f2b24358d58345c720b8a58785b
avinashmadnani05 <avinashmadnani05@gmail.com>
2024-11-02 15:35:29 +0530

    second

--- Client/courseconnector/src/App.js
+++ Client/courseconnector/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Signup from './pages/Signup';
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+// import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    
+    
+    <BrowserRouter>
+    <Routes>
+      <Route path="/signup" element={<Signup />} />
+
+      {/* <Route path="/login" element={<Login />} /> */}
+      {/* <Route path="/UserInput" element={<UserInput />} />
+      <Route path="/domain" element={<DomainInput />} />
+      <Route path="/user" element={<User />} />
+      <Route path="/creators" element={<Creators />} />
+      <Route path="*" element={<HomePage />} /> */}
+    </Routes>
+    </BrowserRouter>
+</>
   );
-}
+};
 
 export default App;
